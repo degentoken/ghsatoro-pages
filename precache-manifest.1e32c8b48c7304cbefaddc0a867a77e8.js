0bf8450d1abde083e9b43f55412e6dea2274924e
DegenDEV <degentoken.finance@gmail.com>
2021-07-04 17:38:12 +0200

    init

---
+++ precache-manifest.1e32c8b48c7304cbefaddc0a867a77e8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45930c1c0c01096407a8d401afd6994b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "228bc95eb65216879e56",
+    "url": "/static/css/main.09e95890.chunk.css"
+  },
+  {
+    "revision": "d2a3546b967a3112fd26",
+    "url": "/static/js/0.7eb0bb14.chunk.js"
+  },
+  {
+    "revision": "41508e5571e0515d151a",
+    "url": "/static/js/10.efaaa9f6.chunk.js"
+  },
+  {
+    "revision": "0fccbc0724d15c1eac36",
+    "url": "/static/js/3.72f92733.chunk.js"
+  },
+  {
+    "revision": "468c1e1d5d24597f1511506e44a10fbf",
+    "url": "/static/js/3.72f92733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021772b6e5e61bcbb023",
+    "url": "/static/js/4.672e150e.chunk.js"
+  },
+  {
+    "revision": "966e3b73f8b8063f99a7",
+    "url": "/static/js/5.252e6132.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/5.252e6132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90eec8711d8c48aa667",
+    "url": "/static/js/6.31c368ad.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.31c368ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cd9e67adb54706a175",
+    "url": "/static/js/7.c7807205.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.c7807205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b664f946b514fa365b6",
+    "url": "/static/js/8.033b7fdc.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.033b7fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8de376a4972b5c81382",
+    "url": "/static/js/9.9c05a290.chunk.js"
+  },
+  {
+    "revision": "228bc95eb65216879e56",
+    "url": "/static/js/main.fda60c3e.chunk.js"
+  },
+  {
+    "revision": "51143a8630a336aa9184",
+    "url": "/static/js/runtime-main.d0a80008.js"
+  },
+  {
+    "revision": "38d74ae5e22ff2845ba1eea7642e54dc",
+    "url": "/static/media/arrow-down.38d74ae5.svg"
+  },
+  {
+    "revision": "1aeef708761941f40cb61d21d24b5f26",
+    "url": "/static/media/binance-wallet.1aeef708.svg"
+  },
+  {
+    "revision": "a0a73714d21557a8cc7f8ffadaac070e",
+    "url": "/static/media/cms.a0a73714.png"
+  },
+  {
+    "revision": "6f06d0e75fc7d61c8bad2cf420ccd405",
+    "url": "/static/media/fCMS-250x250.6f06d0e7.png"
+  },
+  {
+    "revision": "152f1e5a40024aef9173324bf266bc22",
+    "url": "/static/media/metamask-fox.152f1e5a.svg"
+  },
+  {
+    "revision": "4ba1e0a496249536fc86f6934b1505d9",
+    "url": "/static/media/pCMS-250x250.4ba1e0a4.png"
+  },
+  {
+    "revision": "5f96662b115d597a179298dab0762ea2",
+    "url": "/static/media/pill_axuled.5f96662b.png"
+  },
+  {
+    "revision": "a7bc11f7437331cf9ee9fc895aea84b4",
+    "url": "/static/media/pill_trinks.a7bc11f7.png"
+  },
+  {
+    "revision": "1bcaf69b7937d371fc3d9e681a11968a",
+    "url": "/static/media/wallet-connect.1bcaf69b.svg"
+  },
+  {
+    "revision": "21c83ebc491070c77b37055e1b92d59f",
+    "url": "/static/media/wallet_overview_bg.21c83ebc.png"
+  }
+]);
